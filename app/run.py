1fa66ad52ce80625de637a8e1e300e7c9517ecee
Chimenma Goodness <59948754+switnexxtra@users.noreply.github.com>
2025-01-29 17:51:26 +0100

    Update run.py

--- app/run.py
+++ app/run.py
@@ -9,7 +9,7 @@ home = Blueprint('home', __name__, static_folder='static', template_folder='temp
 
 @home.route('/')  
 def index():
-    return render_template('index.html')
+    return render_template('accounts/login.html')
     
 
 if __name__ == "__main__":
