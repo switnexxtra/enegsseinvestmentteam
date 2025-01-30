b8b661eee939d5cd56b26fa2f1d4d88ec942cd39
Chimenma Goodness <59948754+switnexxtra@users.noreply.github.com>
2025-01-30 14:13:06 +0100

    Update extensions.py

--- app/enegsseinvestmentteam/extensions.py
+++ app/enegsseinvestmentteam/extensions.py
@@ -1,6 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 # Initialize SQLAlchemy
 db = SQLAlchemy()
 
+
+# Initialize Flask-Login
+login_manager = LoginManager()
+login_manager.login_view = "accounts.login"  # Redirect unauthorized users to login
